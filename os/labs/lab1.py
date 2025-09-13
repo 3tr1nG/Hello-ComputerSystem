98f704ddef6b1f62c4ac917342cc1bd07d12618c
3tr1nG <1153439347@qq.com>
2025-09-13 18:15:40 +0800

    Update lab1.py

--- os/labs/lab1.py
+++ os/labs/lab1.py
@@ -5,6 +5,11 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from src.process import Process
 
 def sequential_scheduler(procs: list[Process]) -> Process:
-    # TODO: implement a sequential scheduler
-    # Simple implementation: return the first process
-    raise NotImplementedError("sequential_scheduler is not implemented yet")
+    # 顺序调度器实现：返回列表中的第一个进程
+    # 检查进程列表是否为空
+    if procs:
+        return procs[0]
+    # 如果没有进程，返回None
+    return None
+
+
